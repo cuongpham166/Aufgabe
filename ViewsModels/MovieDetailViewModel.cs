2ae7bb064d2da8ede711bd673ae9887907f70313
cuongpham166 <44592100+cuongpham166@users.noreply.github.com>
2020-11-19 14:39:07 +0100

    Add files via upload

---
+++ ViewsModels/MovieDetailViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MovieData.Models;
+
+namespace MovieData.ViewsModels
+{
+    public class MovieDetailViewModel
+    {   public string Value { get;set; }
+        public Movie Movie { get; set; }
+        public Genre Genre { get; set; }
+
+        public List<string> Genres { get; set; }
+    }
+}
